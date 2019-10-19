ee9036823c832c389c46b9ae5f6029d6beedf0c2
LisandroPeixer <lisandropeixer@yahoo.com.br>
2019-10-19 02:27:14 -0300

    CRUD completo

--- src/main/java/com/apiRestful/mongodb/services/UserService.java
+++ src/main/java/com/apiRestful/mongodb/services/UserService.java
@@ -1,12 +1,15 @@
 package com.apiRestful.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.apiRestful.mongodb.domain.User;
+import com.apiRestful.mongodb.dto.UserDto;
 import com.apiRestful.mongodb.repository.UserRepository;
+import com.apiRestful.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +20,32 @@ public class UserService {
 	public List<User>findAll(){
 		return repo.findAll();
 	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {  
+		User newObj = findById(obj.getId());  
+		updateData(newObj, obj);  
+		return repo.save(newObj); 
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+	}
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
+	}
 }
+
